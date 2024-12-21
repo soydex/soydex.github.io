6a1ca6bf61e00ae8f630205d696fdb37bdc20363
soydex <49359256+soydex@users.noreply.github.com>
2024-12-21 16:08:49 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,30 +1,24 @@
-// Configuration
-const snowContainer = document.body; // Ajouter les flocons au body
-const numberOfFlakes = 200; // Nombre total de flocons à afficher
+const snowContainer = document.body; 
+const numberOfFlakes = 200;
 
 function createSnowflake() {
     const snowflake = document.createElement('div');
     snowflake.classList.add('snowflake');
 
-    // Position horizontale aléatoire
     snowflake.style.left = Math.random() * 98 + 'vw';
 
-    // Taille aléatoire
-    const size = Math.random() * 5 + 5; // Entre 5px et 10px
+    const size = Math.random() * 5 + 5; 
     snowflake.style.width = `${size}px`;
     snowflake.style.height = `${size}px`;
 
-    // Durée et délai aléatoires
-    const duration = Math.random() * 3 + 2; // Entre 2s et 5s
+    const duration = Math.random() * 3 + 2; 
     snowflake.style.animationDuration = `${duration}s`;
     snowflake.style.animationDelay = `${Math.random() * 5}s`;
 
-    // Ajouter au body et supprimer après l'animation
     snowContainer.appendChild(snowflake);
     setTimeout(() => {
         snowflake.remove();
     }, duration * 1000);
 }
 
-// Génération continue
-setInterval(createSnowflake, 30); // Un flocon toutes les 300ms
+setInterval(createSnowflake, 30); 
