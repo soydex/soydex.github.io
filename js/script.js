3b93a29551fab057327cf3f5d2074df3ba868e6e
John Doe <johndoe@example.com>
2025-02-27 11:56:59 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
+    button=document.getElementById('dark-mode-button');
+    if (button.innerHTML=="Toggle Dark Mode") {
+        button.innerHTML="Toggle Light Mode";
+    } else {
+        button.innerHTML="Toggle Dark Mode";
+    }
 }
