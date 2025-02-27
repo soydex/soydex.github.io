8e69073a288afbcc733e940be22afef71e13ef4f
John Doe <johndoe@example.com>
2025-02-27 14:34:46 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
     button=document.getElementById('dark-mode-button');
     if (button.innerHTML=="Toggle Dark Mode") {
         button.innerHTML="Toggle Light Mode";
